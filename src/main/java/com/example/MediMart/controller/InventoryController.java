2951414dc347a3e4460b6af3dbdbe9bc28bdd121
Jalpan25 <jalpanvvm@gmail.com>
2025-03-14 19:50:38 +0530

    fully functional backend

--- src/main/java/com/example/MediMart/controller/InventoryController.java
+++ src/main/java/com/example/MediMart/controller/InventoryController.java
@@ -8,7 +8,7 @@ import com.example.MediMart.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
